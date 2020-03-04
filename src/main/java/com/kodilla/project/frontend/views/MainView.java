8714d103530520b06fc8376ea1f891488707ec49
mariusz498 <hajdylamariusz@gmail.com>
2020-03-04 23:42:11 +0100

    WIP

--- src/main/java/com/kodilla/project/frontend/views/MainView.java
+++ src/main/java/com/kodilla/project/frontend/views/MainView.java
@@ -1,11 +1,14 @@
 package com.kodilla.project.frontend.views;
 
 import com.kodilla.project.frontend.client.BackendClient;
+import com.kodilla.project.frontend.domain.CompanyDto;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
@@ -27,7 +30,11 @@ public class MainView extends VerticalLayout {
         companiesButton.addClickListener(e -> {
             UI.getCurrent().navigate(CompanyLoginView.class);
         });
-        trackingButton.addClickListener(e -> backendClient.getCompanies());
+        trackingButton.addClickListener(e -> {
+            List<CompanyDto> companies;
+            companies = backendClient.getCompanies();
+            System.out.println(companies.toString());
+        });
         buttonsLayout.add(deliverersButton);
         buttonsLayout.add(trackingButton);
         return buttonsLayout;
