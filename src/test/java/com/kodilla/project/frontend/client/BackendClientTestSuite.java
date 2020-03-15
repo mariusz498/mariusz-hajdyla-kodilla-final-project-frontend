2e51d149c8d09dd09196a212346680e1ccae7d6d
mariusz498 <hajdylamariusz@gmail.com>
2020-03-15 19:57:31 +0100

    location getting done

---
+++ src/test/java/com/kodilla/project/frontend/client/BackendClientTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.project.frontend.client;
+
+import com.kodilla.project.frontend.domain.LocationDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+    @RunWith(SpringRunner.class)
+    @SpringBootTest
+    public class BackendClientTestSuite {
+
+        @Autowired
+        private BackendClient backendClient;
+
+        @Test
+        public void fetchLocationTest() {
+            //Given
+            String countryCode = "POL";
+            String city = "Kraków";
+            String query = "Myśliwska 40A";
+            //When
+            LocationDto locationDto = backendClient.fetchLocation(countryCode, city, query);
+            System.out.println(locationDto.getLabel());
+        }
+    }
